aee8939135107c84bd704bd50517a48c17d032f1
whiker <whiker@163.com>
2016-02-09 17:17:12 +0800

    init

--- Solution.hpp
+++ Solution.hpp
@@ -1,6 +1,9 @@
 #ifndef SOLUTION
 #define SOLUTION
 
+#include "util.h"
+using namespace std;
+
 class Solution {
 public:
 
---
+++ util.h
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
